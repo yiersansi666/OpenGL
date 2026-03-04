75ce3800a70bed55f13b1c3b7123da0e27a38537
CORLEONE LEE <lwy1143037558@gmail.com>
2026-03-04 23:41:14 +0800

    first commit

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -75,8 +75,8 @@ int main()
 
 	lightingShader.use();
 	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-	model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+	model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+	model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 	lightingShader.setMat4("model", model);
 
 	while (!glfwWindowShouldClose(window))
